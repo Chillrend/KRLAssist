7da9d36c87d0e9c7066cb4ff34c3e810e3ec0b4b
Farhan Hanif <cv2lex@gmail.com>
2019-02-26 20:59:40 +0700

    Initial Commit

---
+++ app/src/main/java/com/a4sc11production/krlassist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.a4sc11production.krlassist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
