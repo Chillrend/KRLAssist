0845f35e8ca15de99faa5aefb1ddd7db16fb29af
Farhan Hanif <cv2lex@gmail.com>
2019-02-27 21:17:41 +0700

    Added app walkthrough (Onboarding Activity) (In-Progress)

--- app/src/main/java/com/a4sc11production/krlassist/walkthrough.java
+++ app/src/main/java/com/a4sc11production/krlassist/walkthrough.java
@@ -3,11 +3,11 @@ package com.a4sc11production.krlassist;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 import com.github.paolorotolo.appintro.model.SliderPage;
 
-public class walkthrough extends AppIntro {
+public class walkthrough extends AppIntro2 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +17,9 @@ public class walkthrough extends AppIntro {
         sliderPage.setTitle("Hello World!");
         sliderPage.setDescription("Prostrate yourself!");
         sliderPage.setImageDrawable(R.drawable.ic_android_24dp);
-        sliderPage.setBgColor(Color.parseColor("#"));
+        sliderPage.setBgColor(Color.parseColor("#f44336"));
         addSlide(AppIntroFragment.newInstance(sliderPage));
+
+        
     }
 }
