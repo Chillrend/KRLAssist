a71b74ac13c463a4d67f058d1ca2db30e4a99f10
Farhan Hanif <cv2lex@gmail.com>
2019-03-13 23:09:50 +0700

    Added app walkthrough (Onboarding Activity)

--- app/src/main/java/com/a4sc11production/krlassist/MainActivity.java
+++ app/src/main/java/com/a4sc11production/krlassist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.a4sc11production.krlassist;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
     }
 }
